d0eae804adcdc6ad1c4319d8606e9340bf0a7230
Fairuz Ahmed Tuna <fairuz.ahmed@brainstation-23.com>
2024-04-25 12:35:24 +0600

    committed all files from experimental-branch

--- basic_program.cpp
+++ basic_program.cpp
@@ -2,20 +2,7 @@
 using namespace std;
 
 int main() {
-    int n;
-    long factorial = 1.0;
-
-    cout << "Enter a positive integer: ";
-    cin >> n;
-
-    if (n < 0)
-        cout << "Error! Factorial of a negative number doesn't exist.";
-    else {
-        for(int i = 1; i <= n; ++i) {
-            factorial *= i;
-        }
-        cout << "Factorial of " << n << " = " << factorial;    
-    }
+    cout<<"HEllo everyone\n";
 
     return 0;
 }
